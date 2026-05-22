6c0b2b0d2b6038b478ebb70038dae6000c9dc78a
Duneth Widanapathirana <153067188+Dune-Wid@users.noreply.github.com>
2026-05-22 14:32:05 +0530

    CV updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import { Download, Eye, Github } from "lucide-react";
-import cvFile from "../assets/Duneth_Widanapathirana_Resume.pdf";
+import cvFile from "../assets/W.P.D.I Sandaruwan_CV.pdf";
 
 export const HeroSection = () => {
     return (
